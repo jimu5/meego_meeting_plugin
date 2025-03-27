59054cdb307e92315d995faf0966a8e2129832ed
jimu5 <853447650@qq.com>
2025-03-27 23:43:30 +0800

    feat: 临时处理 svg 问题

--- fe/src/features/Controls/control-a/form/components/LinkRender/index.tsx
+++ fe/src/features/Controls/control-a/form/components/LinkRender/index.tsx
@@ -1,8 +1,8 @@
 import React, { useMemo } from 'react';
 import { IconLink } from '@douyinfe/semi-icons';
 import { Typography, Image } from '@douyinfe/semi-ui';
-import { LinkType} from '../../../../../../models/types'
-import icon_file_lark from '../../../../../../assets/icon_file_lark.svg'
+import { LinkType } from '../../../../../../models/types';
+import icon_file_lark from '../../../../../../assets/icon_file_lark.png';
 import './index.less';
 import { sdkManager } from '../../../../../../utils';
 
@@ -12,30 +12,31 @@ interface ILinkRender {
 }
 
 const LinkRender = ({ link, type }: ILinkRender) => {
-  const iconMap = useMemo(() => ({
-    [LinkType.MIAOJI]: (
-      <Image
-        className='link-icon'
-        src={icon_file_lark}
-        preview={false}
-      />
-    ),
-    [LinkType.DOC]: <IconLink className='link-icon' />
-  }), [type]);
+  const iconMap = useMemo(
+    () => ({
+      [LinkType.MIAOJI]: <Image className="link-icon" src={icon_file_lark} preview={false} />,
+      [LinkType.DOC]: <IconLink className="link-icon" />,
+    }),
+    [type],
+  );
 
   const onClick = () => {
-    sdkManager.getSdkInstance().then((sdk) => {
+    sdkManager.getSdkInstance().then(sdk => {
       sdk?.navigation?.open(link);
     });
-  }
+  };
 
   // TODO: 根据type显示不同的icon
   return link ? (
-    <Typography.Text className='link-container' link={{}}>
+    <Typography.Text className="link-container" link={{}}>
       {iconMap[type]}
-      <span className='link-text' onClick={onClick}>查看</span>
+      <span className="link-text" onClick={onClick}>
+        查看
+      </span>
     </Typography.Text>
-    ): <span>-</span>;;
+  ) : (
+    <span>-</span>
+  );
 };
 
 export default LinkRender;
